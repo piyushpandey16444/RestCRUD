e0075665daf40f4690188c4193be61bd77a6a000
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-29 14:25:45 +0530

    post request dict created.

--- testing.py
+++ testing.py
@@ -13,4 +13,14 @@ def get_data(id=None):
     print(j_response)
 
 
-get_data()
+# get_data()
+
+def send_data():
+    py_dict = {"id": 6, "name": "Ravi", "roll_no": 6, "city": "MBU"}
+    json_dt = json.dumps(py_dict)
+    json_response = requests.post(url=URL, data=json_dt)
+    j_response = json_response.json()
+    print(j_response)
+
+
+send_data()
