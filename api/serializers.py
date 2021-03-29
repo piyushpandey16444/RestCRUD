b9caea54ff5a05efe1a39ee30fc444fa46004ee1
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-29 14:19:37 +0530

    created student serializer

--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,6 @@ class StudentSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=250)
     roll_no = serializers.IntegerField()
     city = serializers.CharField(max_length=250)
+
+    def create(self, validated_data):
+        return Student.objects.create(**validated_data)
